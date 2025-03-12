765550bb02d42eb3f106076b3a14f2d99b9f4cec
Miguel RV <151050718+MRV1978@users.noreply.github.com>
2025-03-12 17:15:14 +0100

    empiezo los retos

---
+++ roadmap_1_30/sintaxisVariDatos.py
@@ -0,0 +1,35 @@
+# https://python.org
+
+# Comentario de una linea
+
+"""
+Esto también es
+comentario en varias
+lineas
+
+"""
+
+"""
+Esto también es
+comentario en varias
+lineas
+
+"""
+
+my_variable = "Mi variable"
+my_variable = "Mi variable ha cambiado"
+
+
+MY_CONSTANT = "mi constante"  # por convención no se debería de tocar
+MY_CONSTANT = "mi constante también cambia"  # pero se puede cambiar
+
+
+my_int_pero_no_es_necesario_utilizar_nada_especial = 50
+
+print(my_int_pero_no_es_necesario_utilizar_nada_especial)
+print(type(my_int_pero_no_es_necesario_utilizar_nada_especial))
+
+my_int_pero_no_es_necesario_utilizar_nada_especial = "ahora lo cambio"
+
+print(my_int_pero_no_es_necesario_utilizar_nada_especial)
+print(type(my_int_pero_no_es_necesario_utilizar_nada_especial))
