aac48a9c84024f4fce2fab0095d191ecbe5e1394
Miguel RV <151050718+MRV1978@users.noreply.github.com>
2025-03-13 14:35:11 +0100

    terminacion reto funcionesAlcance

--- roadmap_1_30/funcionesAlcance.py
+++ roadmap_1_30/funcionesAlcance.py
@@ -1,3 +1,4 @@
+'''
 """
 funciones definidas por el usuario
 """
@@ -41,6 +42,7 @@ def arg_greet(greet, name):
 arg_greet("Que pasa....", "Miguel")
 arg_greet("Adios", "Carlos")
 arg_greet("Buenos días", "Laura")
+arg_greet(name="Buenos días", greet="Laura")  # Le he cambiado los valores
 
 
 def arg_greet(name, greet="Nos vemos..."):
@@ -49,4 +51,122 @@ def arg_greet(name, greet="Nos vemos..."):
 
 arg_greet("Que pasa....", "Miguel")
 arg_greet("Carlos")
-arg_greet("Laura")
+arg_greet(name="Laura", greet="Adios")
+arg_greet(name="Laura")
+
+
+# con argumento y retorno
+def return_args_greet(greet, name):
+    return f"{greet}, {name}"
+
+
+print(return_args_greet("Como estamos", "tio!"))
+
+# con retorno de varios valores
+
+
+def múltiple_return_greet():
+    return "Hola", "Python"
+
+
+greet, name = múltiple_return_greet()
+print(greet)
+print(name)
+print(greet, name)
+print(f"{greet}, {name}")
+
+
+# con un numero de variables de argumentos
+
+
+def variable_arg_greet(*names):
+    for name in names:
+        print(f"Hola, {name}!")
+
+
+variable_arg_greet("pájaro", "PC", "arbusto", "Miguel")
+
+# con un numero variables de argumentos con palabra clave
+
+
+def variable_key_arg_greet(**names):
+    for key, value in names.items():
+        print(f"Hola, {value} --->({key})")
+
+
+variable_key_arg_greet(animal="pájaro", cosa="PC", planta="arbusto", nombre="Miguel")
+
+"""
+funciones dentro
+de funciones
+"""
+
+
+
+def outer_function():
+    def inner_function():
+        print("Función interna: Como esta el elefante?")
+
+    inner_function()
+
+
+outer_function()
+
+
+"""
+funciones del lenguaje (built-in)
+"""
+print(len("Miguel"))
+print(type("Miguel"))
+print("Miguel".upper())
+
+
+"""
+variables locales y globales
+"""
+
+global_var = "Python"
+print(global_var)
+
+
+def hello_python():
+    local_var = "Hola"
+    print(f"{local_var}, {global_var}")
+
+
+hello_python()
+
+global_var = "Python"
+resultado = None
+
+def hello_python():
+    global resultado
+    local_var = "Hola"
+    resultado = f"{local_var}, {global_var}"
+
+hello_python()
+print(resultado)'
+'''
+
+"""
+extra
+"""
+
+
+def print_numbers(text_1, text_2) -> int:
+    count = 0
+    for number in range(1, 101):
+        if number % 3 == 0 and number % 5 == 0:
+            print(text_1 + text_2)
+        if number % 3 == 0:
+            print(text_1)
+        elif number % 5 == 0:
+            print(text_2)
+        else:
+            print(number)
+            count += 1
+    print("\nCantidad de veces que ha mostrado números:")
+    return count
+
+
+print(print_numbers("Fizz", "Buzz"))
