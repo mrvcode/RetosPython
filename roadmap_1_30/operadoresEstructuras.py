933321546a7b80c8ab74353002808784ec54d5b7
Miguel RV <151050718+MRV1978@users.noreply.github.com>
2025-03-13 07:22:52 +0100

    operadores terminado

--- roadmap_1_30/operadoresEstructuras.py
+++ roadmap_1_30/operadoresEstructuras.py
@@ -37,3 +37,75 @@ print(my_number)
 
 my_new_number = my_number
 print(f"\nmy_new_number is my_new_number es {my_number is my_new_number}")
+print(f"my_new_number is my_new_number es {my_number is not my_new_number}\n")
+
+# operadores de pertenencia
+
+print(
+    f"'x' in 'Miguel' = {'x' in 'Miguel'}"
+)  # en este caso mira si x esta dentro de Miguel
+print(
+    f"'x' in 'Miguel' = {'x' not in 'Miguel'}"
+)  # Es igual que el anterior. Mira si x esta dentro de Miguel
+print(
+    f"'x' in 'Miguel' = {'x' is not 'Miguel'}\n"
+)  # Este caso es diferente ya que comparar la identidad si x es igual a Miguel
+
+"""
+Estructuras de
+Control
+"""
+
+# condicionales
+
+my_string = "MoureDev"
+if my_string == "MoureDev":
+    print("my_string es 'MoureDev'")
+else:
+    print("my_string no es 'MoureDev'")
+
+
+my_string = "MoureDev"
+if my_string == "MoureDeveeeee":
+    print("my_string es 'MoureDev'")
+else:
+    print("my_string no es 'MoureDev'")
+
+
+my_string = "Brais"
+if my_string == "MoureDeveeeee":
+    print("my_string es 'MoureDev'")
+elif my_string == "Brais":
+    print("my_string es 'Brais'")
+else:
+    print("my_string no es 'MoureDev'")
+
+print("\n")
+# iterativas
+for i in range(11):
+    print(i, end=" ")
+print("\n")
+
+i = 0
+
+while i <= 10:
+    print(i, end=" ")
+    i += 1
+print("\n")
+
+# manejo de excepciones
+
+try:
+    print(10 / 1)
+except:
+    print("Se ha producido un error")
+finally:
+    print("Ha finalizado el manejo de excepciones\n")
+
+"""
+Extra
+"""
+
+for number in range(10, 56):
+    if number % 2 == 0 and number != 16 and number % 3 != 0:
+        print(number, end=" ")
