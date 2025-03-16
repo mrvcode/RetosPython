c1bb6af57892523930e77600e289bb33a71df3fe
Miguel RV <151050718+MRV1978@users.noreply.github.com>
2025-03-16 17:54:34 +0100

    termino pilas, añado clases.py

--- roadmap_1_30/pilasColas.py
+++ roadmap_1_30/pilasColas.py
@@ -1,3 +1,4 @@
+"""
 # pila/stack (LIFO)
 
 stack = []
@@ -36,3 +37,63 @@ print(queue)
 # mecanismo propio del lenguaje
 print(queue.pop(0))
 print(queue)
+"""
+
+"""
+extra
+"""
+
+# web
+
+
+def web_navigation():
+
+    stack = []
+
+    while True:
+
+        action = input("Añade una url o interactúa con palabras adelante/atrás/salir: ")
+
+        if action == "salir":
+            print("Saliendo del navegador web.")
+            break
+        elif action == "adelante":
+            pass  # con el elemento stack puro no se puede realizar
+        elif action == "atrás":
+            if len(stack) > 0:
+                stack.pop()
+        else:
+            stack.append(action)
+
+        if len(stack) > 0:
+            print(f"Has navegado a la web: {stack[len(stack) - 1]}")
+        else:
+            print("Estas en la pagina de inicio.")
+
+
+# web_navigation()
+
+
+def share_printed():
+
+    queue = []
+
+    while True:
+
+        action = input("Añade un documento o selecciona imprimir/salir: ")
+
+        if action == "salir":
+            break
+        elif action == "imprimir":
+            if len(queue) > 0:
+                print(f"Imprimiendo: {queue.pop(0)}")
+        else:
+            queue.append(action)
+
+        if len(queue) == 0:
+            print("La cola de impresión esta vacía.")
+        else:
+            print(f"Cola de impresión: {queue}")
+
+
+share_printed()
