360640c0642d1a446eb348c4a001076c6b75f1a9
Miguel RV <151050718+MRV1978@users.noreply.github.com>
2025-03-17 07:13:25 +0100

    termino clases y creo gitignore en la carpeta

---
+++ roadmap_1_30/clases.py
@@ -0,0 +1,65 @@
+class Programmer:
+
+    surname: str = None
+
+    def __init__(self, name: str, age: int, languages: list):
+        self.name = name
+        self.age = age
+        self.languages = languages
+
+    def print(self):
+        print(
+            f"Nombre: {self.name} | Apellidos: {self.surname} | Edad: {self.age} | Lenguajes: {self.languages}"
+        )
+
+
+my_programmer = Programmer("Miguel", 46, ["html", "css", "python"])
+my_programmer.print()
+my_programmer.surname = "Rubio"
+my_programmer.print()
+my_programmer.age = 47
+my_programmer.print()
+print()
+
+"""
+extra
+"""
+
+
+class Stack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, item):
+        self.stack.append(item)
+
+    def pop(self):
+        if self.count() == 0:
+            return None
+        return self.stack.pop()
+
+    def count(self):
+        return len(self.stack)
+
+    def print(self):
+        for item in reversed(self.stack):
+            print(item)
+
+
+my_stack = Stack()
+my_stack.push("A")
+my_stack.push("B")
+my_stack.push("C")
+print(my_stack.count())
+my_stack.print()
+my_stack.pop()
+print(my_stack.count())
+my_stack.print()
+print(my_stack.count())
+print(my_stack.pop())
+print(my_stack.pop())
+print(my_stack.pop())
+print(my_stack.pop())
+print(my_stack.pop())
+print(my_stack.pop())
