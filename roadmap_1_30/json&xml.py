484e331ddd6927175ad4822c60354fa5482f1e7c
Miguel RV <mrv0032@alu.medac.es>
2025-04-07 15:02:30 +0200

    he terminado el rado map 1

--- roadmap_1_30/json&xml.py
+++ roadmap_1_30/json&xml.py
@@ -60,3 +60,39 @@ Extra
 """
 create_xml()
 create_json()
+
+
+class Data:
+
+    def __init__(self, name, age, birthdate, languages):
+        self.name = name
+        self.age = age
+        self.birthdate = birthdate
+        self.languages = languages
+
+
+with open(xml_file, "r") as xml_data:
+    root = xml.fromstring(xml_data.read())
+
+    name = root.find("name").text
+    age = int(root.find("edad").text)
+    birthdate = root.find("birthdate").text
+    programming_languages = []
+    for item in root.find("languages"):
+        programming_languages.append(item.text)
+
+    xml_class = Data(name, age, birthdate, programming_languages)
+    print(xml_class.__dict__)
+
+with open(json_file, "r") as json_data:
+    json_dict = json.load(json_data)
+    json_class = Data(
+        json_dict["name"],
+        json_dict["edad"],
+        json_dict["birthdate"],
+        json_dict["languages"],
+    )
+    print(json_class.__dict__)
+
+os.remove(xml_file)
+os.remove(json_file)
